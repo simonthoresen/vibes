d4aa4172dd11266809bf63cb25e6ec8be445a460
Ronja Hult <ronja@hult-thoresen.com>
2025-08-16 15:07:27 +0200

    Game update

--- 2025-08-10 Ronja/scripts/characters/CharacterData.js
+++ 2025-08-10 Ronja/scripts/characters/CharacterData.js
@@ -4,7 +4,7 @@ const CharacterData = {
             id: 'abaddon',
             name: 'Abaddon',
             description: 'An angel sent to kill every faulty angel, also called fallen demons.',
-            imageUrl: 'images/abaddonC.png',
+            imageUrl: 'images/Chibi/AbaddonC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Abaddon
@@ -14,7 +14,7 @@ const CharacterData = {
             id: 'shade',
             name: 'Shade',
             description: 'The only wolf in a school full of mostly house pets.',
-            imageUrl: 'images/shadeC.png',
+            imageUrl: 'images/Chibi/ShadeC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Shade
@@ -24,7 +24,7 @@ const CharacterData = {
             id: 'dragonwolf',
             name: 'DragonWolf',
             description: 'A mutated half eyed wolf, they were chased out of the half eyed wolves territory and found their family among other outcasts.',
-            imageUrl: 'images/dragonwolfC.png',
+            imageUrl: 'images/Chibi/DragonWolfC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for DragonWolf
@@ -34,7 +34,7 @@ const CharacterData = {
             id: 'safnil',
             name: 'Safnil',
             description: 'A troll who keeps to himself and lashes out to trolls that are under him on the hemospecrum due to the fact that he is not suposed to exist, his is a mutant blood after all.',
-            imageUrl: 'images/safnilC.png',
+            imageUrl: 'images/Chibi/SafnilC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Safnil
@@ -44,7 +44,7 @@ const CharacterData = {
             id: 'kolen',
             name: 'Kolen',
             description: 'A Tabaxi that wears a mask that talks to him. He is untrusting of everyone except the mask.',
-            imageUrl: 'images/kolenC.png',
+            imageUrl: 'images/Chibi/KolenC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Kolen
@@ -54,7 +54,7 @@ const CharacterData = {
             id: 'rolan',
             name: 'Rolan',
             description: 'A half-elf that lives in a village of "misfits" - everyone there has either been deemed misfits by someone in their life or was born there.',
-            imageUrl: 'images/rolanC.png',
+            imageUrl: 'images/Chibi/RolanC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Rolan
@@ -64,7 +64,7 @@ const CharacterData = {
             id: 'teeto',
             name: 'Teeto',
             description: 'A stupid little leopard gecko that accidentally sold his soul to a devil.',
-            imageUrl: 'images/teetoC.png',
+            imageUrl: 'images/Chibi/TeetoC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Teeto
@@ -74,7 +74,7 @@ const CharacterData = {
             id: 'anangel',
             name: 'An Angel',
             description: 'An angel that now lives on earth, their scars a reminder of the past. Although what happened in the past might have been traumatic, they are able to live a comfortable and even happy life now.',
-            imageUrl: 'images/anangelC.png',
+            imageUrl: 'images/Chibi/AnAngelC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Angel
@@ -84,7 +84,7 @@ const CharacterData = {
             id: 'crane',
             name: 'Crane',
             description: 'A crane who works at the big hospital, she loves helping others and is loved by everyone who knows her. A god of memories, at least they used to be.',
-            imageUrl: 'images/CraneC.png',
+            imageUrl: 'images/Chibi/CraneC.png',
             triggerWarning: null,
             worldConfig: {
                 // Specific world configuration for Crane
